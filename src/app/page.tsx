1be8a43001696c3447c8c7f3d087b741bde9c95b
Vinicius Bussolo <vinicius.buss13@gmail.com>
2025-06-02 10:12:46 -0300

    feat: implementando a section de listar os cursos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -143,6 +143,113 @@ export default function Home() {
           ))}
         </div>
       </section>
+
+      <section className="w-full px-2 flex flex-col items-center justify-center mt-36">
+        <div className="w-full max-w-[39.375rem]">
+          <h1 className="font-medium text-3xl text-black text-center">
+            Aprenda com os melhores
+          </h1>
+
+          <p className="text-gray-500 text-center mt-4">
+            Descubra nossos cursos mais bem avaliados em várias categorias. De
+            codificação e design a negócios e bem-estar, nossos cursos são
+            criados para gerar resultados.
+          </p>
+        </div>
+
+        <div className="mt-10 grid grid-cols-4 gap-3.5">
+          <div className="w-full max-w-[16.58rem] flex flex-col items-center pb-12">
+            <Image
+              src={"/images-cards/card-1.svg"}
+              alt="Card 1"
+              width={263.42}
+              height={148}
+              className="object-cover"
+            />
+
+            <div className="w-full max-w-56 space-y-1 mt-2.5">
+              <h4 className="font-semibold leading-5 text-black ">
+                Build Text to image SaaS App in React JS
+              </h4>
+              <p className="text-sm text-gray-500">Richard James</p>
+              <p className="font-medium text-gray-700 text-sm">
+                4.5 <span className="text-gray-500">(122)</span>
+              </p>
+              <p className="font-semibold text-gray-800">R$ 10.99</p>
+            </div>
+          </div>
+
+          <div className="w-full max-w-[16.58rem] flex flex-col items-center pb-12">
+            <Image
+              src={"/images-cards/card-2.svg"}
+              alt="Card 2"
+              width={263.42}
+              height={148}
+              className="object-cover"
+            />
+
+            <div className="w-full max-w-56 space-y-1 mt-2.5">
+              <h4 className="font-semibold leading-5 text-black ">
+                Build Text to image SaaS App in React JS
+              </h4>
+              <p className="text-sm text-gray-500">Richard James</p>
+              <p className="font-medium text-gray-700 text-sm">
+                4.5 <span className="text-gray-500">(122)</span>
+              </p>
+              <p className="font-semibold text-gray-800">R$ 10.99</p>
+            </div>
+          </div>
+
+          <div className="w-full max-w-[16.58rem] flex flex-col items-center pb-12">
+            <Image
+              src={"/images-cards/card-3.svg"}
+              alt="Card 3"
+              width={263.42}
+              height={148}
+              className="object-cover"
+            />
+
+            <div className="w-full max-w-56 space-y-1 mt-2.5">
+              <h4 className="font-semibold leading-5 text-black ">
+                Build Text to image SaaS App in React JS
+              </h4>
+              <p className="text-sm text-gray-500">Richard James</p>
+              <p className="font-medium text-gray-700 text-sm">
+                4.5 <span className="text-gray-500">(122)</span>
+              </p>
+              <p className="font-semibold text-gray-800">R$ 10.99</p>
+            </div>
+          </div>
+
+          <div className="w-full max-w-[16.58rem] flex flex-col items-center pb-12">
+            <Image
+              src={"/images-cards/card-4.svg"}
+              alt="Card 4"
+              width={263.42}
+              height={148}
+              className="object-cover"
+            />
+
+            <div className="w-full max-w-56 space-y-1 mt-2.5">
+              <h4 className="font-semibold leading-5 text-black ">
+                Build Text to image SaaS App in React JS
+              </h4>
+              <p className="text-sm text-gray-500">Richard James</p>
+              <p className="font-medium text-gray-700 text-sm">
+                4.5 <span className="text-gray-500">(122)</span>
+              </p>
+              <p className="font-semibold text-gray-800">R$ 10.99</p>
+            </div>
+          </div>
+        </div>
+
+        <button
+          className="w-60 h-11 mt-11 bg-white text-gray-500 border 
+         border-[#6B7280]/50  rounded-md cursor-pointer"
+        >
+          Mostrar todos os cursos
+        </button>
+      </section>
     </>
   );
 }
